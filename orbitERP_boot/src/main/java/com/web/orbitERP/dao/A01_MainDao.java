e9f6cf1be8ca16a9c088e08751ea0a93037e8562
KNOO <KNOO@odddman>
2024-02-06 00:09:23 +0900

    (24/02/26 00:08) 세션에 부서정보 및 권한뱃지 외

--- orbitERP_boot/src/main/java/com/web/orbitERP/dao/A01_MainDao.java
+++ orbitERP_boot/src/main/java/com/web/orbitERP/dao/A01_MainDao.java
@@ -9,9 +9,10 @@ import com.web.orbitERP.vo.Erpmem;
 // orbitERP.a03_dao.A01_MainDao
 @Mapper
 public interface A01_MainDao {
-	@Select("SELECT m.*, e.ENAME \r\n"
+	@Select("SELECT m.*, e.*, d.dname \r\n"
 			+ "FROM ERPMEM m \r\n"
 			+ "JOIN EMPLOYEE e ON m.EMPNO = e.EMPNO\r\n"
+			+ "JOIN DEPT d ON e.deptno = d.deptno\r\n"
 			+ "WHERE e.EMPNO = #{empno}\r\n"
 			+ "AND m.PWD = #{pwd}")
 	Erpmem login(Erpmem sch);
--- orbitERP_boot/src/main/java/com/web/orbitERP/vo/Erpmem.java
+++ orbitERP_boot/src/main/java/com/web/orbitERP/vo/Erpmem.java
@@ -6,6 +6,9 @@ public class Erpmem {
 	private String ename;
 	private String pwd;
 	private String email;
+	private String deptno;
+	private String dname;
+	
 	public Erpmem() {
 		super();
 		// TODO Auto-generated constructor stub
@@ -17,6 +20,19 @@ public class Erpmem {
 		this.ename = ename;
 		this.pwd = pwd;
 		this.email = email;
+	}
+	
+	public String getDname() {
+		return dname;
+	}
+	public void setDname(String dname) {
+		this.dname = dname;
+	}
+	public String getDeptno() {
+		return deptno;
+	}
+	public void setDeptno(String deptno) {
+		this.deptno = deptno;
 	}
 	public String getEmpno() {
 		return empno;
