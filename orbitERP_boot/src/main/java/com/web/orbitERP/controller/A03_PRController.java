d75526f502525fe6ffc72c757c6344c6f6bc03bb
user <user@DESKTOP-1HITU00>
2024-02-06 14:12:27 +0900

    강의 스케줄러 등록시작

--- orbitERP_boot/src/main/java/com/web/orbitERP/controller/A03_PRController.java
+++ orbitERP_boot/src/main/java/com/web/orbitERP/controller/A03_PRController.java
@@ -98,8 +98,11 @@ public class A03_PRController {
 	}
 	
 	
-	
-	
+	/*--강의 캘린더*------------------------------------------------------------------*/
+	@RequestMapping("lectureCalendar")
+	public String lectureCalendar() {
+		return"a03_planResource\\planCalendar";
+	}
 	
 	
 	
@@ -115,7 +118,7 @@ public class A03_PRController {
 	//캘린더조회
 	@RequestMapping("calendar")
 	public String calendar() {
-		return"a03_planResource\\fullcalendar";
+		return"a03_planResource\\lectureCalendar";
 	}
 	//일정리스트
 	//http://localhost:7080/orbitERP/calList.do
