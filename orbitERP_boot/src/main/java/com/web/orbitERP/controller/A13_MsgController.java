4483e55be05983b2b70426b6c2afa1e195063bcc
shi12 <shi12@LAPTOP-UR8L9TMP>
2024-02-19 23:30:52 +0900

    실시간 메시지 분석

--- orbitERP_boot/src/main/java/com/web/orbitERP/controller/A13_MsgController.java
+++ orbitERP_boot/src/main/java/com/web/orbitERP/controller/A13_MsgController.java
@@ -8,16 +8,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class A13_MsgController {
 	// http://localhost:4444/message
-	@GetMapping("message")
+	@GetMapping("message") //메시지 페이지 열기
 	public String chatting() {
-		return "a22_msg";
+		return "a03_planResource\\a22_msg";
 	}		
 	
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public String greeting(String message) {
     	System.out.println("# 메시지 전송 #"+message);
-    	
+    	//DB저장용임
     	
         return message;
     }
